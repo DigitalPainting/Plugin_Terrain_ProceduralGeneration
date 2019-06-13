4c2fc621fbd54fcd78de29ac3881efadab75eef6
Ross Gardler <rgardler@apache.org>
2019-06-13 00:04:36 -0700

    reduce the terrain generation demo scene to minimal and add an endless terrain demo

---
+++ Assets/Scripts/Wander.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wander : MonoBehaviour
+{
+    public float speed = 5;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, 0, speed * Time.deltaTime);
+    }
+}
